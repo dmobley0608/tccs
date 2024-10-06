da625400e5da21a5dc237d7f0fa9ceaf06482acc
Dwight Mobley <dmobley0608@gmail.com>
2024-10-06 01:08:04 -0400

    fixed static files

--- core/settings.py
+++ core/settings.py
@@ -120,10 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
-   
+    'core/static'
 ]
 
 MEDIA_ROOT = BASE_DIR / "media"
