06f7c6da34112f2d323bc84f01fb1f3c7f1e3133
Dwight Mobley <dmobley0608@gmail.com>
2024-10-07 10:58:24 -0400

    update for ui

--- work_requests/admin.py
+++ work_requests/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
-from .models import WorkOrderRequest
+from .models import WorkOrderRequest, WorkRequestResponse
 
 # Register your models here.
+class ResponseInline(admin.TabularInline):
+    model = WorkRequestResponse
+    
 class WorkOrderRequestAdmin(admin.ModelAdmin):
     list_display=['created_at','customer_name', 'customer_email', 'customer_phone_number', 'status']
     readonly_fields=['created_at', 'updated_at', 'customer_request']
     sortable_by=['created_at', 'customer_name', 'customer_email','status']
+    inlines=[ResponseInline]
+    
 admin.site.register(WorkOrderRequest, WorkOrderRequestAdmin)
--- work_requests/models.py
+++ work_requests/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 from django.urls import reverse
 from enum import Enum
-
+from django.core.mail import send_mail
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
 
 
 class WorkOrderRequest(models.Model):
@@ -30,4 +32,24 @@ class WorkOrderRequest(models.Model):
     def get_absolute_url(self):
         return reverse("WorkOrderRequest_detail", kwargs={"pk": self.pk})
     
-   
+
+class WorkRequestResponse(models.Model):
+    work_request = models.ForeignKey(WorkOrderRequest, on_delete=models.CASCADE)
+    subject = models.CharField(max_length=75)
+    message = models.TextField(blank=True, null=True)   
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def save(self, **kwargs):
+        html_content = render_to_string("email/request-response.html",context={"message":self.message, 'year':2024})            
+        msg = EmailMultiAlternatives(
+            self.subject,
+            html_content,
+            "tim@email.tccs.tech",
+            [self.work_request.customer_email],
+            headers={"List-Unsubscribe": "<mailto:unsub@example.com>"},
+            )
+        msg.attach_alternative(html_content, "text/html")
+        msg.send()
+        super().save(**kwargs)  
+       
